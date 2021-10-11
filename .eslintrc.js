574a1b57daf0847723b99ecb1faea37d3a8cabeb
Ruto <ruto@protonmail.ch>
2021-10-11 16:47:29 -0400

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  env: {
+    node: true,
+    es6: true
+  },
+  extends: 'eslint:recommended',
+  parser: 'babel-eslint',
+  parserOptions: {
+    "sourceType": "module"
+  }
+}
